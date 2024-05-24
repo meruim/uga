0ab3e0feee636a0b9379b5fd56971a6c78df8418
mangroveprot <mangroveprot@gmail.com>
2024-05-24 13:05:28 +0800

    add space

--- frontend/h.js
+++ frontend/h.js
@@ -5,6 +5,21 @@ const header = document.querySelector(".header");
 
 let index = 0;
 
+document.addEventListener("DOMContentLoaded", function () {
+      const navLinks = document.querySelectorAll(".navbar a");
+
+      navLinks.forEach((link) => {
+        link.addEventListener("click", function () {
+          // Remove "active" class from all links
+          navLinks.forEach((link) => link.classList.remove("active"));
+
+          // Add "active" class to the clicked link
+          this.classList.add("active");
+        });
+      });
+    });
+    
+
 // JavaScript to toggle the
 // visibility of the navigation menu
 document.addEventListener("DOMContentLoaded", function () {
